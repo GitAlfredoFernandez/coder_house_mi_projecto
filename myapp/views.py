d16610f8bb8dbbaeda7a5b72b257f6ca55c25555
AF <alfredofernandez@gmail.com>
2025-01-27 12:19:14 -0300

    Django Project added

---
+++ myapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request): 
+    contexto = {'mensaje': 'Hola Django - Coder'} 
+    return render(request, 'myapp/index.html', contexto)
