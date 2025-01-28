bf23960a0658c5e321b910b76244874bca51684b
AF <alfredofernandez@gmail.com>
2025-01-27 22:21:07 -0300

    prueba

--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request): 
+    
     contexto = {'mensaje': 'Hola Django - Coder'} 
     return render(request, 'myapp/index.html', contexto)
